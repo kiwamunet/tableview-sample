e2dae34370f411ad688b09f152f2d6f3c52fa0bf
suzuki_kiwamu <suzuki_kiwamu@cyberagent.co.jp>
2014-09-01 09:59:31 +0900

    first commit

---
+++ tableview-sample/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  tableview-sample
+//
+//  Created by suzuki_kiwamu on 9/1/14.
+//  Copyright (c) 2014 suzuki_kiwamu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
